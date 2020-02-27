bbb838dce807f9de3f47255674fdb3083b7fc158
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2020-02-27 16:53:04 +0300

    feat: add 2nd data lvl to types table

--- src/data.py
+++ src/data.py
@@ -1,37 +1,80 @@
 data = {
   'dv': {
     'name': 'ДВ',
-    'rubber-1': [
+    'material': [
       {
-        'type': 'ДВ-170/12',
-        'movements': {
-          'unstrech': 7,
-          'strech': 12,
-          'shift_v': 7,
-          'shift_h': 3,
-        },
-        'pressure': 0.47
+        'mat_type': 'Резина',
+        'mat_type_ext': 'Резиновые (EPDM)',
+        'types': [
+          {
+            'type': 'ДВ-170/12',
+            'movements': {
+              'unstrech': 7,
+              'strech': 12,
+              'shift_v': 7,
+              'shift_h': 3,
+            },
+            'pressure': 0.47
+          },
+          {
+            'type': 'ДВ-220/25',
+            'movements': {
+              'unstrech': 15,
+              'strech': 25,
+              'shift_v': 15,
+              'shift_h': 10,
+            },
+            'pressure': 0.58
+          },
+          {
+            'type': 'ДВ-270/25',
+            'movements': {
+              'unstrech': 15,
+              'strech': 25,
+              'shift_v': 15,
+              'shift_h': 10,
+            },
+            'pressure': 0.75
+          }
+        ]
       },
       {
-        'type': 'ДВ-220/25',
-        'movements': {
-          'unstrech': 15,
-          'strech': 25,
-          'shift_v': 15,
-          'shift_h': 10,
-        },
-        'pressure': 0.58
+        'mat_type': 'Резина',
+        'mat_type_ext': 'Резиновые (SBS)',
+        'types': [
+          {
+            'type': 'ДВ-170/12-S',
+            'movements': {
+              'unstrech': 7,
+              'strech': 12,
+              'shift_v': 7,
+              'shift_h': 3,
+            },
+            'pressure': 0.47
+          },
+          {
+            'type': 'ДВ-220/25-S',
+            'movements': {
+              'unstrech': 15,
+              'strech': 25,
+              'shift_v': 15,
+              'shift_h': 10,
+            },
+            'pressure': 0.58
+          },
+          {
+            'type': 'ДВ-270/25-S',
+            'movements': {
+              'unstrech': 15,
+              'strech': 25,
+              'shift_v': 15,
+              'shift_h': 10,
+            },
+            'pressure': 0.75
+          }
+        ]
       },
-      {
-        'type': 'ДВ-270/25',
-        'movements': {
-          'unstrech': 15,
-          'strech': 25,
-          'shift_v': 15,
-          'shift_h': 10,
-        },
-        'pressure': 0.75
-      }
     ]
+    
   }   
 }
